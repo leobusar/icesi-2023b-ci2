10701e9ab07b5e6acd3db1a440f280b7109e0ea9
Leonardo Bustamante <leobusta@gmail.com>
2023-10-03 14:20:35 -0500

    add axios

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,34 +1,74 @@
-import {useState} from 'react'
-import tasks from  '../data/todos.json'
+import {useEffect, useState} from 'react'
+//import tasks from  '../data/todos.json'
+import axios from  '../config/axios'
 import TaskTable from '../components/TaskTable'
 import TaskForm from '../components/TaskForm'
 import PropTypes from 'prop-types'
 
 function TaskList({owner}) {
 
-  const [taskList, setTaskList] = useState(tasks)
+  const [taskList, setTaskList] = useState([])
   const [taskEdit, setTaskEdit] = useState({id:"", title:"", completed: false, userId:""})
 
-  const addTask = (task) => {
-    let taskTmp = [...taskList]
+  const getTasks = async () => {
+    try {
+      /*axios.get("/tasks")
+        .then ( (res)=> {
+          console.log(res.data)
+          setTaskList(res.data)
+        })
+        */
+       const res = await axios.get("/tasks")
+       setTaskList(res.data)
+    }catch(e){
+      console.log(e)
+    }
+  }
+
+  useEffect( () => {getTasks()}, [])
+
+  const addTask = async (task) => {
+//    let taskTmp = [...taskList]
 
     if (task.id==""){
       task.id= Math.floor(Math.random()*100000)
-      taskTmp.push(task)
+      //taskTmp.push(task)
+      try{
+        const res = await axios.post("/tasks", task)
+        if(res.status==201)
+          getTasks()
+      }catch (e){
+        console.log(e)
+      }
     }else{
-      let index = taskList.findIndex(elem => elem.id==task.id)
-      taskTmp[index] = task
+      //let index = taskList.findIndex(elem => elem.id==task.id)
+      //taskTmp[index] = task
+      try{
+        const res = await axios.put("/tasks/"+task.id, task)
+        if(res.status==200)
+          getTasks()
+      }catch (e){
+        console.log(e)
+      }
+
     }  
-    setTaskList(taskTmp)
+    //setTaskList(taskTmp)
   }
 
-  const delTask =  (id) => {
-    let index = taskList.findIndex(task => task.id==id)
-    taskList.splice(index,1);
-    let taskTmp = [...taskList]
+  const delTask = async (id) => {
+    // let index = taskList.findIndex(task => task.id==id)
+    // taskList.splice(index,1);
+    // let taskTmp = [...taskList]
 
-    setTaskList(taskTmp)
+    // setTaskList(taskTmp)
     //setTaskList(taskList.filter(task => task.id!=id))
+    try {
+      const res = await axios.delete("/tasks/"+id)
+      if(res.status==200)
+        getTasks()
+    }catch (e){
+      console.log(e)
+    }
   }
 
   return (
